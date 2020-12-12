68b24213544fc48423cae706ff4b990329676c0c
SeongjaeJeong <jsj90@robotics.snu.ac.kr>
2020-12-12 18:13:48 +0900

    json import 관련 테스트 추가

---
+++ Ch1/test/test_import_json.py
@@ -0,0 +1,16 @@
+import unittest
+from utils import import_json_file
+
+
+class TestImportJson(unittest.TestCase):
+    def test_imported_invoice_not_none(self):
+        invoice = import_json_file("./invoices.json")
+        self.assertIsNotNone(invoice)
+
+    def test_imported_plays_not_none(self):
+        plays = import_json_file("plays.json")
+        self.assertIsNotNone(plays)
+
+
+if __name__ == "__main__":
+    unittest.main()
