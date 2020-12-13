4d078fab204df7225b9d0200e6032531a5215a44
SeongjaeJeong <jsj90@robotics.snu.ac.kr>
2020-12-13 19:41:26 +0900

    playFor 함수가 enrichPerformance에서만 사용되도록 변경

--- Ch1/statement.py
+++ Ch1/statement.py
@@ -1,18 +1,28 @@
 import math
+import copy
 
 
 def statement(invoice: dict, plays: dict) -> str:
     statement_data = dict()
     statement_data["customer"] = invoice["customer"]
-    statement_data["performances"] = invoice["performances"]
+    statement_data["performances"] = [
+        enrichPerformance(perf, plays) for perf in invoice["performances"]
+    ]
+
     return renderPlainText(statement_data, plays)
+
+
+def enrichPerformance(aPerformance, plays):
+    result = copy.copy(aPerformance)
+    result["play"] = playFor(result, plays)
+    return result
 
 
 def renderPlainText(data, plays):
     result = f'청구 내역 (고객명: {data["customer"]})\n'
     for perf in data["performances"]:
         # 청구 내역 출력
-        result += f'\t{playFor(perf, plays)["name"]}: ${usd(amountFor(perf, plays))} ({perf["audience"]}석)\n'
+        result += f'\t{perf["play"]["name"]}: ${usd(amountFor(perf, plays))} ({perf["audience"]}석)\n'
     result += f"총액: ${usd(totalAmount(data, plays))}\n"
     result += f"적립 포인트: {totalVolumeCredits(data, plays)}점"
     return result
@@ -40,7 +50,7 @@ def usd(aNumber):
 def volumeCreditsFor(aPerformance, plays):
     result = 0
     result += max(aPerformance["audience"] - 30, 0)
-    if playFor(aPerformance, plays)["type"] == "comedy":
+    if aPerformance["play"]["type"] == "comedy":
         result += math.floor(aPerformance["audience"] / 5)
     return result
 
@@ -51,15 +61,15 @@ def playFor(aPerformance, plays):
 
 def amountFor(aPerformance, plays):
     result = 0
-    if playFor(aPerformance, plays)["type"] == "tragedy":
+    if aPerformance["play"]["type"] == "tragedy":
         result = 40000
         if aPerformance["audience"] > 30:
             result += 1000 * (aPerformance["audience"] - 30)
-    elif playFor(aPerformance, plays)["type"] == "comedy":
+    elif aPerformance["play"]["type"] == "comedy":
         result = 30000
         if aPerformance["audience"] > 20:
             result += 10000 + 500 * (aPerformance["audience"] - 20)
         result += 300 * aPerformance["audience"]
     else:
-        raise Exception(f'알 수 없는 장르: {playFor(aPerformance, plays)["type"]}')
+        raise Exception(f'알 수 없는 장르: {aPerformance["play"]["type"]}')
     return result
